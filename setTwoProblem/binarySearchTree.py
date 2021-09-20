1cee6a896cac05f8c66c24d892deb3772692b3a5
Manish Reddy Bendhi <manishreddybendhi@Manishs-MBP.attlocal.net>
2021-09-19 18:52:32 -0600

    adding recusion for binary sarch

--- setTwoProblem/binarySearchTree.py
+++ setTwoProblem/binarySearchTree.py
@@ -1,32 +1,22 @@
-def binary_search(cards,query):
-    #defining start and ed of the list, starting at zero and end is the last element in this case end mins one
-    start = 0; high = len(cards) -1
+class TreeNode:
+    def __init__(self,key):
+        self.key = key
+        self.left = None
+        self.right = None
 
-    while start <= high:
-        # looks at the middle element to see if we have to traverse left of right
-        mid = (start + high) // 2
 
-        # variable helps us to debug
-        print('start', start, 'high', high, 'query', query, 'mid', mid)
-
-        # if the search element is the middle one, the we found it. We can return the value
-
-        if query == cards[mid]:
-            return mid
-        # if the search element is greater than middle value we have to check for the next element in the array,
-        # we are going to iterate by one value
-        elif query > cards[mid]:
-            start = mid + 1
-        # in case the query is lesser than mid, we have to traverse to the left
-        elif cards[mid] > query:
-            high = mid - 1
+node0 = TreeNode(3)
+node1 = TreeNode(4)
+node2 = TreeNode(5)
+tree = node0
 
 
 
+node0.left = node1
+node0.right = node2
 
-dictTest = {
-    'input':{'cards':[2,4,6,8,9,12,15],'query':12},
-    'output':3
-}
-result = binary_search(dictTest['input']['cards'],dictTest['input']['query'])
-print(result)
+
+print(node0.key)
+print(tree.key)
+print(tree.left.key)
+print(tree.right.key)
