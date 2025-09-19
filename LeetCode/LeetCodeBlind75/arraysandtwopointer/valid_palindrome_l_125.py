96194f6f5f1b771e1b9e515a1b5a4a3e76e1f8aa
Manish B <manishb@Mac.lan>
2025-09-18 22:15:59 -0500

    pushing code

--- LeetCode/LeetCodeBlind75/arraysandtwopointer/valid_palindrome_l_125.py
+++ LeetCode/LeetCodeBlind75/arraysandtwopointer/valid_palindrome_l_125.py
@@ -1,18 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
-
-        while l < r:
-            if not self.alphaNum(s[l]):
-                l = l+1
-            if not self.alphaNum(s[r]):
-                r = r-1
-            elif s[l].lower() != s[r].lower():
+         l, r = 0, len(s) - 1
+         while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
                 return False
-            else:
-                l = l+1
-                r = r-1
-        return True
+            l, r = l + 1, r - 1
+            return True
+
+ 
 
     def alphaNum(self, c):
         return ord('A') <= ord(c) <= ord('Z') or \
